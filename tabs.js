e5a800cc8f01d7b0de8f948ed4ddc861a8e5a64f
FoadSL <mfoadsalmani@gmail.com>
2023-12-29 15:39:38 +0330

    Refactored javascript and fixed a css bug

--- tabs.js
+++ tabs.js
@@ -1,23 +1,39 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-console.log(Array.from(tabs));
-
 let tabFocus = 0;
 
-const changeContent = function (targetTab) {
-  const mainContainer = targetTab.parentElement.parentElement;
-  const tabPanels = mainContainer.querySelectorAll('[role="tabpanel"');
-  const pictures = mainContainer.querySelectorAll("picture");
+const hideContent = function (container, selector) {
+  container
+    .querySelectorAll(selector)
+    .forEach((item) => item.setAttribute("hidden", ""));
+};
+
+const showContent = function (container, selector) {
+  container.querySelector(selector).removeAttribute("hidden");
+};
+
+const changeTabPanel = function (targetTab) {
+  const tabContainer = targetTab.parentElement;
+  const mainContainer = tabContainer.parentElement;
 
   const targetPanel = targetTab.getAttribute("aria-controls");
   const targetImage = targetTab.getAttribute("data-image");
 
-  pictures.forEach((picture) => picture.setAttribute("hidden", ""));
-  mainContainer.querySelector(`#${targetImage}`).removeAttribute("hidden");
+  hideContent(mainContainer, "picture");
+  showContent(mainContainer, `#${targetImage}`);
 
-  tabPanels.forEach((tab) => tab.setAttribute("hidden", ""));
-  mainContainer.querySelector(`#${targetPanel}`).removeAttribute("hidden");
+  hideContent(mainContainer, '[role="tabpanel"]');
+  showContent(mainContainer, `#${targetPanel}`);
+
+  const curTab = tabContainer.querySelector('[aria-selected="true"]');
+
+  // curTab.setAttribute("tabindex", "-1");
+  curTab.setAttribute("aria-selected", "false");
+
+  // targetTab.setAttribute("tabindex", "0");
+  targetTab.setAttribute("aria-selected", "true");
+  // targetTab.focus();
 };
 
 const changeTabFocus = function (e) {
@@ -28,40 +44,26 @@ const changeTabFocus = function (e) {
   if (e.keyCode === keyDownLeft || e.keyCode === keyDownRight) {
     e.target.setAttribute("tabindex", "-1");
     e.target.setAttribute("aria-selected", "false");
+
+    if (e.keyCode === keyDownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) tabFocus = 0;
+    }
+
+    if (e.keyCode === keyDownLeft) {
+      tabFocus--;
+      if (tabFocus <= -1) tabFocus = tabs.length - 1;
+    }
+
+    tabs[tabFocus].setAttribute("tabindex", "0");
+    tabs[tabFocus].setAttribute("aria-selected", "true");
+    tabs[tabFocus].focus();
   }
-
-  if (e.keyCode === keyDownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) tabFocus = 0;
-  }
-
-  if (e.keyCode === keyDownLeft) {
-    tabFocus--;
-    if (tabFocus <= -1) tabFocus = tabs.length - 1;
-  }
-
-  tabs[tabFocus].setAttribute("tabindex", "0");
-  tabs[tabFocus].setAttribute("aria-selected", "true");
-  tabs[tabFocus].focus();
-
-  changeContent(tabs[tabFocus]);
 };
 
 tabList.addEventListener("keydown", changeTabFocus);
 
 tabList.addEventListener("click", function (e) {
   const targetTab = e.target.closest('[role="tab"]');
-  if (!targetTab) return;
-
-  const prevTab = Array.from(tabs).find(
-    (tab) => tab.attributes.tabindex.value === "0"
-  );
-  prevTab.setAttribute("tabindex", "-1");
-  prevTab.setAttribute("aria-selected", "false");
-
-  targetTab.setAttribute("tabindex", "0");
-  targetTab.setAttribute("aria-selected", "true");
-  targetTab.focus();
-
-  changeContent(targetTab);
+  if (targetTab) changeTabPanel(targetTab);
 });
